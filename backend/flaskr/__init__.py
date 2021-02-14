296bbc6c4f78f12dc22ea2781378545dcf43692e
Mohamed EL-Khouly <msirag95@gmail.com>
2021-02-14 08:48:12 +0200

    added cors instance

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -12,11 +12,9 @@ def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__)
     setup_db(app)
-  
-    '''
-        @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
-    '''
-
+    
+    #Intiate CORS
+    CORS(app)
     '''
         @TODO: Use the after_request decorator to set Access-Control-Allow
     '''
